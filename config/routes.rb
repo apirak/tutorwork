f8f7571265ed08ed589c62f3db81f1c896ef5d1d
Apirak Panatkool <apirakb@gmail.com>
2023-11-06 05:54:08 +0700

    move branches to school

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :branches
-  resources :schools
+  resources :schools do
+    resources :branches
+  end
   resources :parents
   resources :curriculums
   resources :teachers
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
