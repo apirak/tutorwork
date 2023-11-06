4058a6146038ddcb1f4a33002fbb2bf37914f520
Apirak Panatkool <apirakb@gmail.com>
2023-11-06 08:50:09 +0700

    update test

--- test/controllers/branches_controller_test.rb
+++ test/controllers/branches_controller_test.rb
@@ -1,21 +1,24 @@
 require "test_helper"
 
 class BranchesControllerTest < ActionDispatch::IntegrationTest
-  setup { @branch = branches(:one) }
+  setup do
+    @school = schools(:one)
+    @branch = branches(:one)
+  end
 
   test "should get index" do
-    get branches_url
+    get school_branches_url(@school)
     assert_response :success
   end
 
   test "should get new" do
-    get new_branch_url
+    get new_school_branch_url(@school)
     assert_response :success
   end
 
   test "should create branch" do
     assert_difference("Branch.count") do
-      post branches_url,
+      post school_branches_url(@school),
            params: {
              branch: {
                name: @branch.name,
@@ -24,33 +27,35 @@ class BranchesControllerTest < ActionDispatch::IntegrationTest
            }
     end
 
-    assert_redirected_to branch_url(Branch.last)
+    assert_redirected_to school_branch_url(@school, Branch.last)
   end
 
   test "should show branch" do
-    get branch_url(@branch)
+    get school_branch_url(@school, @branch)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_branch_url(@branch)
+    get edit_school_branch_url(@school, @branch)
     assert_response :success
   end
 
   test "should update branch" do
-    patch branch_url(@branch),
+    patch school_branch_url(@school, @branch),
           params: {
             branch: {
               name: @branch.name,
               school_id: @branch.school_id,
             },
           }
-    assert_redirected_to branch_url(@branch)
+    assert_redirected_to school_branch_url(@school, @branch)
   end
 
   test "should destroy branch" do
-    assert_difference("Branch.count", -1) { delete branch_url(@branch) }
+    assert_difference("Branch.count", -1) do
+      delete school_branch_url(@school, @branch)
+    end
 
-    assert_redirected_to branches_url
+    assert_redirected_to school_branches_url(@school)
   end
 end
